7c6263b25c88e7bf4750f761e0b27ee436eea2cf
Shaan P <shaanp@umich.edu>
2022-12-20 16:23:51 -0500

    Got some kind of Node relationship setup in the databse, need to write the views now

--- concept_map/models.py
+++ concept_map/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Node(models.Model):
+    text = models.CharField(max_length=256, default="")
+    connections = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.text
+
+
+# class Relationship(models.Model):
+#     # node_from = models.IntegerField(null=False)
+#     # node_to = models.IntegerField(null=False)
+#     # (node_from, node_to) = models.AutoField(primary_key=True)
+#     start_node = models.ForeignKey(Node, on_delete=models.CASCADE)
+#     end_node = models.ForeignKey(Node, on_delete=models.CASCADE)
+
+#     # def __str__(self):
+#     #     return f"{self.node_from} to {self.node_to}"
